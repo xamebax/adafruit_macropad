5be125679cb32a62396a34fd1bcf642a8e221744
Marta Paciorkowska <marta.paciorkowska@oda.com>
2022-12-14 14:40:34 +0100

    Use first pass at a Programmers Dvorak keyboard layout (some keycodes missing)

--- code.py
+++ code.py
@@ -24,158 +24,289 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_base import KeyboardLayoutBase
 
-# Layout below is a copy from "https://github.com/Neradoc/Circuitpython_Keyboard_Layouts.git"
+# Note: this KeyboardLayout was generated using https://github.com/Neradoc/Circuitpython_Keyboard_Layouts
+# and a modified XML file downloaded from https://kbdlayout.info/KBDDV/download/xml
+# the XML above is for a US Dvorak layout; the layout below is for a Polish Programmer's Dvorak layout.
+# TODO: there are no keycodes available for characters:
+# - " (34/0x22)
+# - ' (39/0x27)
+# - $ (36/0x24)
+# - 'ą' (261/0x105) and other Polish and Norwegian characters (no alt layer)
 class KeyboardLayout(KeyboardLayoutBase):
-    """Map ASCII characters to appropriate keypresses on a standard US PC keyboard.
-    Non-ASCII characters and most control characters will raise an exception.
-    """
-    # The ASCII_TO_KEYCODE bytes object is used as a table to maps ASCII 0-127
-    # to the corresponding # keycode on a US 104-key keyboard.
-    # The user should not normally need to use this table,
-    # but it is not marked as private.
-    #
-    # Because the table only goes to 127, we use the top bit of each byte (ox80) to indicate
-    # that the shift key should be pressed. So any values 0x{8,9,a,b}* are shifted characters.
-    #
-    # The Python compiler will concatenate all these bytes literals into a single bytes object.
-    # Micropython/CircuitPython will store the resulting bytes constant in flash memory
-    # if it's in a .mpy file, so it doesn't use up valuable RAM.
-    #
-    # \x00 entries have no keyboard key and so won't be sent.
     SHIFT_FLAG = 0x80
     ASCII_TO_KEYCODE = (
-        b"\x00"  # NUL
-        b"\x00"  # SOH
-        b"\x00"  # STX
-        b"\x00"  # ETX
-        b"\x00"  # EOT
-        b"\x00"  # ENQ
-        b"\x00"  # ACK
-        b"\x00"  # BEL \a
-        b"\x2a"  # BS BACKSPACE \b (called DELETE in the usb.org document)
-        b"\x2b"  # TAB \t
-        b"\x28"  # LF \n (called Return or ENTER in the usb.org document)
-        b"\x00"  # VT \v
-        b"\x00"  # FF \f
-        b"\x00"  # CR \r
-        b"\x00"  # SO
-        b"\x00"  # SI
-        b"\x00"  # DLE
-        b"\x00"  # DC1
-        b"\x00"  # DC2
-        b"\x00"  # DC3
-        b"\x00"  # DC4
-        b"\x00"  # NAK
-        b"\x00"  # SYN
-        b"\x00"  # ETB
-        b"\x00"  # CAN
-        b"\x00"  # EM
-        b"\x00"  # SUB
-        b"\x29"  # ESC
-        b"\x00"  # FS
-        b"\x00"  # GS
-        b"\x00"  # RS
-        b"\x00"  # US
-        b"\x2c"  # SPACE
-        b"\x9e"  # !
-        b"\x94"  # "
-        b"\xa0"  # #
-        b"\xa1"  # $
-        b"\xa2"  # %
-        b"\xa4"  # &
-        b"\x14"  # '
-        b"\xa6"  # (
-        b"\xa7"  # )
-        b"\xa5"  # *
-        b"\xb0"  # +
-        b"\x1a"  # ,
-        b"\x34"  # -
-        b"\x08"  # .
-        b"\x2f"  # /
-        b"\x27"  # 0
-        b"\x1e"  # 1
-        b"\x1f"  # 2
-        b"\x20"  # 3
-        b"\x21"  # 4
-        b"\x22"  # 5
-        b"\x23"  # 6
-        b"\x24"  # 7
-        b"\x25"  # 8
-        b"\x26"  # 9
-        b"\x9d"  # :
-        b"\x1d"  # ;
-        b"\x9a"  # <
-        b"\x30"  # =
-        b"\x88"  # >
-        b"\xaf"  # ?
-        b"\x9f"  # @
-        b"\x84"  # A
-        b"\x91"  # B
-        b"\x8c"  # C
-        b"\x8b"  # D
-        b"\x87"  # E
-        b"\x9c"  # F
-        b"\x98"  # G
-        b"\x8d"  # H
-        b"\x8a"  # I
-        b"\x86"  # J
-        b"\x99"  # K
-        b"\x93"  # L
-        b"\x90"  # M
-        b"\x8f"  # N
-        b"\x96"  # O
-        b"\x95"  # P
-        b"\x92"  # R
-        b"\x9b"  # Q
-        b"\xb3"  # S
-        b"\x8e"  # T
-        b"\x89"  # U
-        b"\xb7"  # V
-        b"\xb6"  # W
-        b"\x85"  # X
-        b"\x97"  # Y
-        b"\xb8"  # Z
-        b"\x2d"  # [
-        b"\x31"  # \ backslash
-        b"\x2e"  # ]
-        b"\xa3"  # ^
-        b"\xb4"  # _
-        b"\x35"  # `
-        b"\x04"  # a
-        b"\x11"  # b
-        b"\x0c"  # c
-        b"\x0b"  # d
-        b"\x07"  # e
-        b"\x1c"  # f
-        b"\x18"  # g
-        b"\x0d"  # h
-        b"\x0a"  # i
-        b"\x06"  # j
-        b"\x19"  # k
-        b"\x13"  # l
-        b"\x10"  # m
-        b"\x0f"  # n
-        b"\x16"  # o
-        b"\x15"  # p
-        b"\x1b"  # q
-        b"\x12"  # r
-        b"\x33"  # s
-        b"\x0e"  # t
-        b"\x09"  # u
-        b"\x37"  # v
-        b"\x36"  # w
-        b"\x05"  # x
-        b"\x17"  # y
-        b"\x38"  # z
-        b"\xad"  # {
-        b"\xb1"  # |
-        b"\xae"  # }
-        b"\xb5"  # ~
-        b"\x4c"  # DEL DELETE (called Forward Delete in usb.org document)
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x2a'  # BACKSPACE
+        b'\x2b'  # '\t'
+        b'\x28'  # '\n'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x29'  # ESC
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x2c'  # ' '
+        b'\x2d'  # '!'
+        b'\x00'
+        b'\x2e'  # '#'
+        b'\x00'
+        b'\x9e'  # '%'
+        b'\x1e'  # '&'
+        b'\x00'
+        b'\x22'  # '('
+        b'\x25'  # ')'
+        b'\x24'  # '*'
+        b'\x26'  # '+'
+        b'\x1a'  # ','
+        b'\x34'  # '-'
+        b'\x08'  # '.'
+        b'\x2f'  # '/'
+        b'\xa4'  # '0'
+        b'\xa2'  # '1'
+        b'\xa5'  # '2'
+        b'\xa1'  # '3'
+        b'\xa6'  # '4'
+        b'\xa0'  # '5'
+        b'\xa7'  # '6'
+        b'\x9f'  # '7'
+        b'\xad'  # '8'
+        b'\xa3'  # '9'
+        b'\x94'  # ':'
+        b'\x14'  # ';'
+        b'\x9a'  # '<'
+        b'\x23'  # '='
+        b'\x88'  # '>'
+        b'\xaf'  # '?'
+        b'\x30'  # '@'
+        b'\x84'  # 'A'
+        b'\x91'  # 'B'
+        b'\x8c'  # 'C'
+        b'\x8b'  # 'D'
+        b'\x87'  # 'E'
+        b'\x9c'  # 'F'
+        b'\x98'  # 'G'
+        b'\x8d'  # 'H'
+        b'\x8a'  # 'I'
+        b'\x86'  # 'J'
+        b'\x99'  # 'K'
+        b'\x93'  # 'L'
+        b'\x90'  # 'M'
+        b'\x8f'  # 'N'
+        b'\x96'  # 'O'
+        b'\x95'  # 'P'
+        b'\x9b'  # 'Q'
+        b'\x92'  # 'R'
+        b'\xb3'  # 'S'
+        b'\x8e'  # 'T'
+        b'\x89'  # 'U'
+        b'\xb7'  # 'V'
+        b'\xb6'  # 'W'
+        b'\x85'  # 'X'
+        b'\x97'  # 'Y'
+        b'\xb8'  # 'Z'
+        b'\x1f'  # '['
+        b'\x31'  # '\\'
+        b'\x27'  # ']'
+        b'\xb0'  # '^'
+        b'\xb4'  # '_'
+        b'\xae'  # '`'
+        b'\x04'  # 'a'
+        b'\x11'  # 'b'
+        b'\x0c'  # 'c'
+        b'\x0b'  # 'd'
+        b'\x07'  # 'e'
+        b'\x1c'  # 'f'
+        b'\x18'  # 'g'
+        b'\x0d'  # 'h'
+        b'\x0a'  # 'i'
+        b'\x06'  # 'j'
+        b'\x19'  # 'k'
+        b'\x13'  # 'l'
+        b'\x10'  # 'm'
+        b'\x0f'  # 'n'
+        b'\x16'  # 'o'
+        b'\x15'  # 'p'
+        b'\x1b'  # 'q'
+        b'\x12'  # 'r'
+        b'\x33'  # 's'
+        b'\x0e'  # 't'
+        b'\x09'  # 'u'
+        b'\x37'  # 'v'
+        b'\x36'  # 'w'
+        b'\x05'  # 'x'
+        b'\x17'  # 'y'
+        b'\x38'  # 'z'
+        b'\x20'  # '{'
+        b'\xb1'  # '|'
+        b'\x21'  # '}'
+        b'\xb5'  # '~'
+        b'\x00'
     )
+    NEED_ALTGR = ''
+    HIGHER_ASCII = {
+    }
+    COMBINED_KEYS = {
+    }
+
+class Keycode:
+    A = 0x04
+    B = 0x11
+    C = 0x0c
+    D = 0x0b
+    E = 0x07
+    F = 0x1c
+    G = 0x18
+    H = 0x0d
+    I = 0x0a
+    J = 0x06
+    K = 0x19
+    L = 0x13
+    M = 0x10
+    N = 0x0f
+    O = 0x16
+    P = 0x15
+    Q = 0x1b
+    R = 0x12
+    S = 0x33
+    T = 0x0e
+    U = 0x09
+    V = 0x37
+    W = 0x36
+    X = 0x05
+    Y = 0x17
+    Z = 0x38
+    ALT = 0xe2
+    END = 0x4d
+    F1 = 0x3a
+    F2 = 0x3b
+    F3 = 0x3c
+    F4 = 0x3d
+    F5 = 0x3e
+    F6 = 0x3f
+    F7 = 0x40
+    F8 = 0x41
+    F9 = 0x42
+    F10 = 0x43
+    F11 = 0x44
+    F12 = 0x45
+    F13 = 0x68
+    F14 = 0x69
+    F15 = 0x6a
+    F16 = 0x6b
+    F17 = 0x6c
+    F18 = 0x6d
+    F19 = 0x6e
+    F20 = 0x6f
+    F21 = 0x70
+    F22 = 0x71
+    F23 = 0x72
+    F24 = 0x73
+    GUI = 0xe3
+    ONE = 0x1e
+    SIX = 0x23
+    TAB = 0x2b
+    TWO = 0x1f
+    FIVE = 0x22
+    FOUR = 0x21
+    HOME = 0x4a
+    NINE = 0x26
+    ZERO = 0x27
+    ALTGR = 0xe6
+    COMMA = 0x1a
+    EIGHT = 0x25
+    ENTER = 0x28
+    MINUS = 0x34
+    PAUSE = 0x48
+    QUOTE = 0x14
+    SEVEN = 0x24
+    SHIFT = 0xe1
+    SPACE = 0x2c
+    THREE = 0x20
+    APPLICATION = 0x65
+    BACKSLASH = 0x31
+    BACKSPACE = 0x2a
+    CAPS_LOCK = 0x39
+    COMMAND = 0xe3
+    CONTROL = 0xe0
+    DELETE = 0x4c
+    DOWN_ARROW = 0x51
+    EQUALS = 0x30
+    ESCAPE = 0x29
+    FORWARD_SLASH = 0x2f
+    GRAVE_ACCENT = 0x35
+    INSERT = 0x49
+    KEYPAD_ASTERISK = 0x55
+    KEYPAD_EIGHT = 0x60
+    KEYPAD_FIVE = 0x5d
+    KEYPAD_FORWARD_SLASH = 0x54
+    KEYPAD_FOUR = 0x5c
+    KEYPAD_MINUS = 0x56
+    KEYPAD_NINE = 0x61
+    KEYPAD_NUMLOCK = 0x53
+    KEYPAD_ONE = 0x59
+    KEYPAD_PERIOD = 0x63
+    KEYPAD_PLUS = 0x57
+    KEYPAD_SEVEN = 0x5f
+    KEYPAD_SIX = 0x5e
+    KEYPAD_THREE = 0x5b
+    KEYPAD_TWO = 0x5a
+    KEYPAD_ZERO = 0x62
+    LEFT_ALT = 0xe2
+    LEFT_ARROW = 0x50
+    LEFT_BRACKET = 0x2d
+    LEFT_CONTROL = 0xe0
+    LEFT_GUI = 0xe3
+    LEFT_SHIFT = 0xe1
+    OEM_102 = 0x64
+    OPTION = 0xe2
+    PAGE_DOWN = 0x4e
+    PAGE_UP = 0x4b
+    PERIOD = 0x08
+    PRINT_SCREEN = 0x46
+    RETURN = 0x28
+    RIGHT_ALT = 0xe6
+    RIGHT_ARROW = 0x4f
+    RIGHT_BRACKET = 0x2e
+    RIGHT_CONTROL = 0xe4
+    RIGHT_GUI = 0xe7
+    RIGHT_SHIFT = 0xe5
+    SCROLL_LOCK = 0x47
+    SEMICOLON = 0x1d
+    SPACEBAR = 0x2c
+    UP_ARROW = 0x52
+    WINDOWS = 0xe3
+
+    @classmethod
+    def modifier_bit(cls, keycode):
+        """Return the modifer bit to be set in an HID keycode report if this is a
+        modifier key; otherwise return 0."""
+        return (
+            1 << (keycode - 0xE0) if cls.LEFT_CONTROL <= keycode <= cls.RIGHT_GUI else 0
+        )
 
 # END Keyboard layout -----------------------------------------------------
-
 
 # CONFIGURABLES ------------------------
 
