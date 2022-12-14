75471a53cc82bdad24d97e4aa0bff4c8b138d9f9
Marta Paciorkowska <marta.paciorkowska@oda.com>
2022-12-14 14:45:27 +0100

    Move some actions into methods of their own

--- code.py
+++ code.py
@@ -352,41 +352,46 @@ keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayout(keyboard)
 
 # Set up displayio group with all the labels
-group = displayio.Group()
-for key_index in range(12):
-    x = key_index % 3
-    y = key_index // 3
-    group.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
-                             anchored_position=((macropad.display.width - 1) * x / 2,
-                                                macropad.display.height - 1 -
-                                                (3 - y) * 12),
-                             anchor_point=(x / 2, 1.0)))
-group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
-group.append(label.Label(terminalio.FONT, text='', color=0x000000,
-                         anchored_position=(macropad.display.width//2, -2),
-                         anchor_point=(0.5, 0.0)))
-macropad.display.show(group)
+def display_setup(group):
+    for key_index in range(12):
+        x = key_index % 3
+        y = key_index // 3
+        group.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
+                                 anchored_position=((macropad.display.width - 1) * x / 2,
+                                                    macropad.display.height - 1 -
+                                                    (3 - y) * 12),
+                                 anchor_point=(x / 2, 1.0)))
+    group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
+    group.append(label.Label(terminalio.FONT, text='', color=0x000000,
+                             anchored_position=(macropad.display.width//2, -2),
+                             anchor_point=(0.5, 0.0)))
+    macropad.display.show(group)
 
 # Load all the macro key setups from .py files in MACRO_FOLDER
-apps = []
-files = os.listdir(MACRO_FOLDER)
-files.sort()
-for filename in files:
-    if filename.endswith('.py'):
-        try:
-            module = __import__(MACRO_FOLDER + '/' + filename[:-3])
-            apps.append(App(module.app))
-        except (SyntaxError, ImportError, AttributeError, KeyError, NameError,
-                IndexError, TypeError) as err:
-            print("ERROR in", filename)
-            import traceback
-            traceback.print_exception(err, err, err.__traceback__)
+def load_macros(apps):
+    files = os.listdir(MACRO_FOLDER)
+    files.sort()
+    for filename in files:
+        if filename.endswith('.py'):
+            try:
+                module = __import__(MACRO_FOLDER + '/' + filename[:-3])
+                apps.append(App(module.app))
+            except (SyntaxError, ImportError, AttributeError, KeyError, NameError,
+                    IndexError, TypeError) as err:
+                print("ERROR in", filename)
+                import traceback
+                traceback.print_exception(err, err, err.__traceback__)
+    if not apps:
+        group[13].text = 'NO MACRO FILES FOUND'
+        macropad.display.refresh()
+        while True:
+            pass
 
-if not apps:
-    group[13].text = 'NO MACRO FILES FOUND'
-    macropad.display.refresh()
-    while True:
-        pass
+
+group = displayio.Group()
+display_setup(group)
+apps = []
+load_macros(apps)
 
 last_position = None
 last_encoder_switch = macropad.encoder_switch_debounced.pressed
